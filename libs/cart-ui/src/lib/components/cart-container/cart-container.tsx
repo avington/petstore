8e7017847f3c7eca45d1c5c0a92cc31a9d823308
Steven Moseley <smoseley@anthology.com>
2022-07-29 11:31:29 -0400

    add usestate example

--- libs/cart-ui/src/lib/components/cart-container/cart-container.tsx
+++ libs/cart-ui/src/lib/components/cart-container/cart-container.tsx
@@ -1,28 +1,28 @@
+import { useState } from 'react';
+
 import styles from './cart-container.module.scss';
 
 /* eslint-disable-next-line */
 export interface CartContainerProps {}
 
 export function CartContainer(props: CartContainerProps) {
+  const [cart, setCart] = useState([
+    { id: '1', name: 'Zebra', amount: 100, total: 1 },
+    { id: '2', name: 'Moose', amount: 50, total: 1 },
+    { id: '3', name: 'Dog', amount: 25, total: 5 },
+  ]);
+
   return (
     <div className={styles['container']}>
       <h2>Shopping Cart</h2>
       <div className={styles['cart-list']}>
-        <div className={styles['cart-item']}>
-          <div className={styles['name']}>Zebra</div>
-          <div className={styles['amount']}>100</div>
-          <div className={styles['total']}>1</div>
-        </div>
-        <div className={styles['cart-item']}>
-          <div className={styles['name']}>Moose</div>
-          <div className={styles['amount']}>50</div>
-          <div className={styles['total']}>1</div>
-        </div>
-        <div className={styles['cart-item']}>
-          <div className={styles['name']}>Doc</div>
-          <div className={styles['amount']}>25</div>
-          <div className={styles['total']}>5</div>
-        </div>
+        {cart.map((item) => (
+          <div className={styles['cart-item']}>
+            <div className={styles['name']}>{item.name}</div>
+            <div className={styles['amount']}>{item.amount}</div>
+            <div className={styles['total']}>{item.total}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
